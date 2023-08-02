2d7a5202ee9fc217081b615dc0e5ed6ad34c2686
Oluwatimilehin <70233949+OLUWATIMILEHIN-THEOPHILUS@users.noreply.github.com>
2023-08-02 12:45:17 +0100

    Added methods of swapping two variables in python

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -17,3 +17,27 @@ elif N % 5 == 0:
 else:
     print('N is neither divisible by 3 nor 5')
 
+
+    
+# Swapping two variables in python
+    (Note that method 1 & 2 only work for numbers likewise consume more/extra bit, unlike method 3
+which works for both numbers and strings and consumes less bit)
+        (Note; you can also do this by setting a third variable)
+a = 14
+b = 43
+
+# Method 1
+# a = a + b
+# b = a - b
+# a = a - b
+
+# Method 2 (using XOR)
+a = a ^ b
+b = a ^ b
+a = a ^ b
+
+# Method 3
+# a,b = b,a
+
+print(a)
+print(b)
